46ed5ebd99e70173858c08d7c19ba629b0910512
MihajloMilojevic <milojevicm374@gmail.com>
2026-03-13 13:08:24 +0100

    #1 fix: update plugin imports and adjust project metadata

--- src/nessie_npm_dependencies_plugin/__init__.py
+++ src/nessie_npm_dependencies_plugin/__init__.py
@@ -1,5 +1,3 @@
-from .plugin import npm_dependencies_plugin
+from nessie_npm_dependencies_plugin.plugin import npm_dependencies_plugin
 
-__all__ = [
-    "npm_dependencies_plugin"
-]
+__all__ = ["npm_dependencies_plugin"]
--- src/nessie_npm_dependencies_plugin/plugin.py
+++ src/nessie_npm_dependencies_plugin/plugin.py
@@ -1,21 +1,21 @@
-from nessie_api.models import Action, plugin
 import asyncio
-from .fetch import build_dependency_graph
-from typing import Any
-from nessie_api.models import Graph
 
-def load_graph(action: Action) -> Graph:
+from nessie_api.models import Action, plugin, SetupRequirementType, Graph
+from nessie_api.protocols import Context
+from nessie_npm_dependencies_plugin.fetch import build_dependency_graph
+
+def load_graph(action: Action, context: Context) -> Graph:
     return asyncio.run(build_dependency_graph(action.payload["Package Name"]))
 
 
 @plugin("NPM Package Dependencies")
 def npm_dependencies_plugin():
     handlers = {
-        "load": load_graph,
+        "load_graph": load_graph,
     }
     requires = []
     setup_requires = {
-        "Package Name": str
+        "Package Name": SetupRequirementType.STRING
     }
 
     ret_dict = {
